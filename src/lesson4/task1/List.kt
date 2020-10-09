a9bf3be66c6cd1935a5d10db5d441c954493bc19
RegularSadness <shamanovms@mail.ru>
2020-10-09 09:39:20 +0300

    Done

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -120,14 +120,29 @@ fun buildSumExample(list: List<Int>) = list.joinToString(separator = " + ", post
  * по формуле abs = sqrt(a1^2 + a2^2 + ... + aN^2).
  * Модуль пустого вектора считать равным 0.0.
  */
-fun abs(v: List<Double>): Double = TODO()
+fun abs(v: List<Double>): Double {
+    var a = 0.0
+    for (i in v.indices) {
+        a += v[i] * v[i]
+    }
+    return sqrt(a)
+}
 
 /**
  * Простая (2 балла)
  *
  * Рассчитать среднее арифметическое элементов списка list. Вернуть 0.0, если список пуст
  */
-fun mean(list: List<Double>): Double = TODO()
+fun mean(list: List<Double>): Double {
+    var x = 0.0
+    var y = 0.0
+    if (list.isEmpty()) return 0.0
+    for (element in list) {
+        x += element
+        y++
+    }
+    return x / y
+}
 
 /**
  * Средняя (3 балла)
@@ -137,7 +152,13 @@ fun mean(list: List<Double>): Double = TODO()
  *
  * Обратите внимание, что данная функция должна изменять содержание списка list, а не его копии.
  */
-fun center(list: MutableList<Double>): MutableList<Double> = TODO()
+fun center(list: MutableList<Double>): MutableList<Double> {
+    val average = list.average()
+    for (i in list.indices) {
+        list[i] -= average
+    }
+    return list
+}
 
 /**
  * Средняя (3 балла)
@@ -186,7 +207,11 @@ fun factorize(n: Int): List<Int> = TODO()
  * Результат разложения вернуть в виде строки, например 75 -> 3*5*5
  * Множители в результирующей строке должны располагаться по возрастанию.
  */
-fun factorizeToString(n: Int): String = TODO()
+fun factorizeToString(n: Int): String {
+    val list = factorize(n)
+    return list.joinToString(separator = "*")
+}
+
 
 /**
  * Средняя (3 балла)
